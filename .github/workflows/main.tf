a4d8b76a1566496b23bacacb8fa4d690209679f7
anvika16 <gcpcloudtest1992@gmail.com>
2024-11-28 16:49:20 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+provider google{
+    project = rich-discovery-441812-s6
+    region  = us-east1
+    zone    = us-east1-a
+} 
+
+
+
+
 resource "google_storage_bucket" "my-bucket" {
   name          = "my_bucket_veera"
   location      = "US"
