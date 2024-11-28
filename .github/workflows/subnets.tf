f9c852763599b73ce1a7201afc61fc3dc9414588
anvika16 <gcpcloudtest1992@gmail.com>
2024-11-28 17:29:54 +0530

    Update subnets.tf

--- .github/workflows/subnets.tf
+++ .github/workflows/subnets.tf
@@ -1,11 +1,11 @@
-resource "google_compute_subnetwork" "my-subnet" {
+resource "google_compute_subnetwork" "test-subnet" {
   name          = "my-subnet"
   ip_cidr_range = "10.1.0.0/24"
   region        = "us-east1"
   network       = "custom-vpc"  # Specify the VPC network
 }
 
-resource "google_compute_subnetwork" "my-subnet" {
+resource "google_compute_subnetwork" "test-subnet" {
   name          = "my-subnet-1"
   ip_cidr_range = "10.2.0.0/24"
   region        = "us-east4"
