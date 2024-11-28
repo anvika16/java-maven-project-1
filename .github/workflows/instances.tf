0e02054aeff751c7aceecf7a8dec4068f644ada2
anvika16 <gcpcloudtest1992@gmail.com>
2024-11-28 18:22:29 +0530

    Create instances.tf

---
+++ .github/workflows/instances.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "e2-medium"
+  zone         = "us-east1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "my-subnet"
+    access_config {}
+  }
+}
